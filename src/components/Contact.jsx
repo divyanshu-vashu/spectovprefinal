a7d9e9699acd09208f6c2dba1dbad400e46139ec
Divyanshu Singh <89933176+divyanshu-vashu@users.noreply.github.com>
2024-06-07 14:58:43 +0530

    email js Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,21 +33,21 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-        {
-          from_name: form.name,
-          to_name: "SpectoV",
-          from_email: form.email,
-          to_email: "vdkalife@gmail.com",
-          message: form.message,
-        },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+      {
+        from_name: form.name,
+        to_name: "SpectoV",
+        from_email: form.email,
+        to_email: import.meta.env.VITE_EMAILJS_TO_EMAIL,
+        message: form.message,
+      },
+        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
       )
       .then(
         () => {
           setLoading(false);
-          alert("Thank you. I will get back to you as soon as possible.");
+          alert("Thank you for reaching to us. We will get back to you as soon as possible😊.");
 
           setForm({
             name: "",
