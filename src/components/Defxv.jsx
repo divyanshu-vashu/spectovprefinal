8c5dc96a4e5d5037eba2de961942e9a1ace68eca
Komal Suryanarayana <100284626+komalsuryanarayana@users.noreply.github.com>
2024-06-12 14:27:02 +0530

    Update Defxv.jsx

--- src/components/Defxv.jsx
+++ src/components/Defxv.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { SectionWrapper } from "../hoc";
 import { Link } from 'react-router-dom';
+import '../Styles/Defxv.css';
 
 const Defxv = () => {
   return (
-    <div className="containerdefxv h-full mx-auto w-full">
+    <div className="containerdefxv">
       <div className="left-content">
         <div className="defxvh1">
           <h1 className="text-white">
             Your H1 Text Here Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, assumenda.
           </h1>
-        
-        <div className="def-btn">
-        <Link to="/products"><button className="learn-more-btn ">Learn More</button></Link>
-        </div>
+          <div className="def-btn">
+            <Link to="/products"><button className="learn-more-btn ">Learn More</button></Link>
+          </div>
         </div>
       </div>
       <div className="right-content">
@@ -22,5 +22,5 @@ const Defxv = () => {
     </div>
   );
 };
+
 export default SectionWrapper(Defxv, "defxv");
-// export default Defxv;
