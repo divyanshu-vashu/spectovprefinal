1f31a5f008f2c0b14d796982a3e7290dab8789a5
Komal Suryanarayana <100284626+komalsuryanarayana@users.noreply.github.com>
2024-06-14 12:53:35 +0530

    Update Defxv.jsx

--- src/components/Defxv.jsx
+++ src/components/Defxv.jsx
@@ -5,16 +5,16 @@ import '../Styles/Defxv.css';
 
 const Defxv = () => {
   return (
-    <div className="roww">
-      <div className="leftt">
-        <h1 className="contentt">
+    <div className="custom-row">
+      <div className="custom-left">
+        <h1 className="custom-content">
           Your H1 Text Here Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, assumenda.
         </h1>
-        <div className="def-btnn">
-          <Link to="/products"><button className="learn-more-btn ">Learn More</button></Link>
+        <div className="custom-def-btn">
+          <Link to="/products"><button className="custom-learn-more-btn">Learn More</button></Link>
         </div>
     </div>
-      <div className="rightt">
+      <div className="custom-right">
         <img src="/glasses2.png" alt="alternative_text" />
       </div>
     </div>
