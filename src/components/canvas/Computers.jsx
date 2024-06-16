8c4d981c4a20a001cbcfbef8f300542ced0eabdd
Divyanshu Singh <89933176+divyanshu-vashu@users.noreply.github.com>
2024-06-16 21:26:48 +0530

    Update Computers.jsx

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,9 +5,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  // const computer = useGLTF("./oculus_quest_vr_headset/scene.gltf");
-  // const computer = useGLTF("./virtual_reality_headset_millionpainter/scene.gltf");
-  // const computer = useGLTF("./apple_vision_pro/scene.gltf");
+ 
   const computer = useGLTF("./wayfarer_frame/scene.gltf");
 
   return (
@@ -80,47 +78,3 @@ const ComputersCanvas = () => {
 };
 
 export default ComputersCanvas;
-// import React, { Suspense } from "react";
-// import { Canvas } from "@react-three/fiber";
-// import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
-
-// import CanvasLoader from "../Loader";
-
-// const Earth = () => {
-//   const earth = useGLTF("./apple_vision_pro/scene.gltf");
-
-//   return (
-//     <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
-//   );
-// };
-
-// const EarthCanvas = () => {
-//   return (
-//     <Canvas
-//       shadows
-//       frameloop='demand'
-//       dpr={[1, 2]}
-//       gl={{ preserveDrawingBuffer: true }}
-//       camera={{
-//         fov: 45,
-//         near: 0.1,
-//         far: 200,
-//         position: [-4, 3, 6],
-//       }}
-//     >
-//       <Suspense fallback={<CanvasLoader />}>
-//         <OrbitControls
-//           autoRotate
-//           enableZoom={false}
-//           maxPolarAngle={Math.PI / 2}
-//           minPolarAngle={Math.PI / 2}
-//         />
-//         <Earth />
-
-//         <Preload all />
-//       </Suspense>
-//     </Canvas>
-//   );
-// };
-
-// export default EarthCanvas;
