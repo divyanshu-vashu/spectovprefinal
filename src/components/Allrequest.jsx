fce625d4dd4a1d05b8d431fca92f4d5dfe3a86ed
Rajeev Ranjan <97228502+mrrajeev29@users.noreply.github.com>
2024-06-23 11:09:17 +0530

    Add files via upload

---
+++ src/components/Allrequest.jsx
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import "../Styles/allRequest.css";
+
+function UsersList() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/api/all-request');
+        setUsers(response.data);
+        setLoading(false);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  const handleSubmit = async (e, email, courseIndex) => {
+    e.preventDefault();
+   // alert(`User ID: ${userId}, Course Index: ${courseIndex}`);
+    try {
+      await axios.put(`http://localhost:8080/api/enroll/permission/${email}/${courseIndex}`, { index: courseIndex });
+      alert('Enrollment Successful.');
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleSubmitReject = async (e, email, courseIndex) => {
+    e.preventDefault();
+   // alert(`User ID: ${userId}, Course Index: ${courseIndex}`);
+    try {
+      await axios.put(`http://localhost:8080/api/reject/permission/${email}/${courseIndex}`, { index: courseIndex });
+      alert('You have rejected this enrollment.');
+      window.location.reload()
+    } catch (error) {
+      console.log(error);
+    }
+  };
+ 
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
+  const filteredUsers = users.filter(user => user.courses.includes("pending"));
+
+  return (
+    <div className='parent'>
+      <div className="container">
+        <h1 className="header">Pending Requests</h1>
+        {filteredUsers.length === 0 ? (
+          <p className="no-users">No users with pending courses found.</p>
+        ) : (
+          filteredUsers.map(user => (
+            <div key={user._id} className="user-card">
+              <h2 className="user-name">{user.firstName} {user.lastName}</h2>
+              <p className="user-email">Email: {user.email}</p>
+              <h3 className="section-title">Pending Courses:</h3>
+              <ul className="course-list">
+                {user.courses.map((course, index) => (
+                  course === 'pending' && (
+                    <li key={index} className="course-item">
+                      <p className="course-status">Course Status:&emsp;&emsp;&emsp; {course}</p>
+                      <p className="course-transaction">Transaction Id:&emsp;&emsp;&emsp; {user.transaction[index]}</p>
+                      <p className="course-name">Course Name:&emsp;&emsp;&emsp; {user.coursename[index]}</p>
+                      <button className="approve-button" onClick={(e) => handleSubmit(e, user.email, index,)}>Approve</button>
+                      <button className="reject-button"  onClick={(e) => handleSubmitReject(e, user.email, index,)}>Reject</button>
+                    </li>
+                  )
+                ))}
+              </ul>
+            </div>
+          ))
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default UsersList;
---
+++ src/components/Main.jsx
@@ -0,0 +1,128 @@
+import CareerCard from "../components/CareerCard";
+import { Navbar } from "../components/";
+import ctestimg from "../assets/careerCardTestImage.png";
+import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import axios from 'axios';
+
+
+let careers = [
+	{
+	  id: 0,
+	  title: "Full Stack Development",
+	  subtitle: "Learn Full Stack Development",
+	  content: "Learn Full Stack Development content",
+	  img: ctestimg,
+	},
+	{
+	  id: 1,
+	  title: "Artificial Intelligence and Machine Learning",
+	  subtitle: "Artificial Intelligence and Machine Learning",
+	  content: "Artificial Intelligence and Machine Learning",
+	  img: ctestimg,
+	},
+	{
+	  id: 2,
+	  title: "AR VR",
+	  subtitle: "Artificial Intelligence and Machine Learning",
+	  content: "Artificial Intelligence and Machine Learning",
+	  img: ctestimg,
+	},
+	{
+	  id: 3,
+	  title: "Logic Building and DSA",
+	  subtitle: "Artificial Intelligence and Machine Learning",
+	  content: "Artificial Intelligence and Machine Learning",
+	  img: ctestimg,
+	},
+	{
+	  id: 4,
+	  title: "DSA And Web ",
+	  subtitle: "Artificial Intelligence and Machine Learning",
+	  content: "Artificial Intelligence and Machine Learning",
+	  img: ctestimg,
+	},
+	{
+	  id: 5,
+	  title: "DSA And ML",
+	  subtitle: "Artificial Intelligence and Machine Learning",
+	  content: "Artificial Intelligence and Machine Learning",
+	  img: ctestimg,
+	},
+	{
+	  id: 6,
+	  title: "SpectoV Special",
+	  subtitle: "Artificial Intelligence and Machine Learning",
+	  content: "Artificial Intelligence and Machine Learning",
+	  img: ctestimg,
+	},
+	{
+	  id: 7,
+	  title: "AR VR",
+	  subtitle: "Artificial Intelligence and Machine Learning",
+	  content: "Artificial Intelligence and Machine Learning",
+	  img: ctestimg,
+	},
+  ];
+  
+const Main = () => {
+
+	//const name=localStorage.getItem('name')
+	//alert(name)
+
+
+	const [user, setUser] = useState([]);
+	const email = localStorage.getItem("email");
+	const getCodeDetail=async()=>{
+	  try{
+		  const {data}=await axios.get(`http://localhost:8080/api/details/${email}`);
+			  setUser(data);
+  
+	  }catch(error){
+		  console.log(error);
+  }}
+  //alert(user.courses)
+	  getCodeDetail();
+	 localStorage.setItem('courses',user.courses)
+
+	 const handleLogout = () => {
+		localStorage.removeItem("token");
+		window.location.reload();
+	};
+	return (
+		<> 
+		<Navbar />
+		<div style={{display:"flex",justifyContent:"space-between"}}>
+		<h1 className="ml-5 mt-32 text-4xl font-bold text-black md:ml-24">
+		  Careers At SpectoV
+		</h1>
+		<div style={{display:"flex"}}>
+		<h1 className="mt-32">Hi {user.firstName}</h1>&emsp;
+
+		<Link to="/details" className="mt-32"  >
+				<button style={{backgroundColor:"pink",color:"white",height:"2.5rem",borderRadius:"10px"}} >&emsp;Profile&emsp;</button>
+			  </Link>      &emsp;
+		<Link to="/login" className="mt-32" >
+				<button style={{backgroundColor:"pink",color:"white",height:"2.5rem",borderRadius:"10px"}}  onClick={handleLogout}>&emsp;logout&emsp;</button>
+			  </Link>&emsp;</div>
+		</div>
+	
+		<div className="w-100 m-10 ml-5 flex flex-wrap md:ml-20">
+		  {careers.map((career) => (
+			<CareerCard
+			  key={career.id}
+			  item={career.id}
+			  title={career.title}
+			  subtitle={career.subtitle}
+			  content={career.content}
+			  img={career.img}
+			  //courses={user.courses}
+			/>
+		  ))}
+		</div>
+	  </>
+
+	);
+};
+
+export default Main;
